c4a6fb1a904311ab7ec90ac3c16a45b3fd064e0d
Руслан Водяницкий <Russlansen@gmail.com>
2017-12-19 15:47:56 +0300

    Created new project

---
+++ ElateTableFramework/ElateTableFramework/TableHelper.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+namespace ElateTableFramework
+{
+    public static class TableHelper
+    {
+        public static MvcHtmlString ElateTable(this HtmlHelper html)
+        {
+            TagBuilder table = new TagBuilder("table");
+            return new MvcHtmlString(table.ToString());
+        }
+    }
+}
